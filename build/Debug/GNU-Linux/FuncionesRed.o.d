fdf4be72d04628c177f98535fc722e4adcf35bfb
Oscar Guarnizo <oscar.guarnizo@yachaytech.edu.ec>
2018-04-12 18:30:13 -0500

    Creacion del cableado entre redes. Edite el main para presentarlo de otra manera para observar los resultados por el momento. Las funciones esta comentadas en FuncionesSuperRed.c.

---
+++ build/Debug/GNU-Linux/FuncionesRed.o.d
@@ -0,0 +1,6 @@
+build/Debug/GNU-Linux/FuncionesRed.o: FuncionesRed.c structs.h \
+ functions.h
+
+structs.h:
+
+functions.h:
--- main.c
+++ main.c
@@ -22,39 +22,45 @@
 int main(int argc, char** argv) {
     
     /*Definicion de parametros globales*/
-    int K = 4, N= 10, numReds = 2;
-    float p = 0.5, r = 0.1;
+    int K = 4, N= 10, numReds = 2,numConx=2;
+    float p = 0.5, r = 0.1,q=0.01; //q aun no estamos usando revisar las funciones superRed
     
     
     Red red1 = {0,N,K,p,r};
     Red red2 = {1,N,K,p,r};
-//    Red red3 = {2,N,K,p,r};
 
-    
+    //Inicializo las redes
     inicializarRed(&red1);
-//    inicializarRed(&red3);
-    cableadoInicial(&red1,numReds);
-//    cableadoInicial(&red3,numReds);
-    imprimirRed(&red1,numReds);
-    printf("\n--------------------------------------\n");
-    recablearRed(&red1,numReds);
-    imprimirRed(&red1,numReds);
-
-    printf("\n--------------------------------------\n");
-    printf("\n--------------------------------------\n");
-
-    
     inicializarRed(&red2);
-    cableadoInicial(&red2,numReds);
-    imprimirRed(&red2,numReds);
+    
+    
+    
     printf("\n--------------------------------------\n");
-    recablearRed(&red2, numReds);
+    printf("\nCABLEADO INICIAL\n");
+    
+    //Realizo el cableado inicial
+    cableadoInicial(&red1,numReds);
+    cableadoInicial(&red2,numReds);
+    imprimirRed(&red1,numReds);
     imprimirRed(&red2,numReds);
-//    imprimirRed(&red3,numReds);
 
+    printf("\n--------------------------------------\n");
+    printf("\nRECABLEADO POR SEPARADO\n");
     
-//    entrelazarRedes(&Red1,&Red2);
+    //Recableo ambas redes por separado
+    recablearRed(&red1,numReds);
+    recablearRed(&red2, numReds);
+    imprimirRed(&red1,numReds);
+    imprimirRed(&red2,numReds);
     
+    printf("\n--------------------------------------\n");
+    printf("\nRECABLEADO DE AMBAS REDES\n");
+
+    recablearDosRedes(&red1,&red2,numConx,numReds,q);
+    imprimirRed(&red1,numReds);
+    imprimirRed(&red2,numReds);
+    
+        
     return (EXIT_SUCCESS);
 }
 
